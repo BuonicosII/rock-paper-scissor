db9bed07f7cf1574f8d1eae1a8a4f7bd3bddeb15
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-01-03 20:50:19 +0100

    Creata e testata funzione round di gioco

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     if (randomChoice === 1) {
-        return "Sasso"
+        return "sasso"
     } else if (randomChoice === 2) {
-        return "Carta"
+        return "carta"
     } else {
-        return "Forbici"
+        return "forbici"
     }
 }
 
@@ -20,10 +20,31 @@ function getComputerChoice() {
 //tre messaggi
 
 function playRound() {
-    const playerSelection = prompt("Sasso, carta o forbici?", " ");
+    let askPlayer = prompt("Sasso, carta o forbici?", " ");
+    const playerSelection = (String(askPlayer)).toLowerCase();
     const computerSelection = getComputerChoice();
-}
 
+    switch(true) {
+        case (playerSelection !== "sasso" && playerSelection !== "forbici" && playerSelection !== "carta"):
+            return "Inserisci una delle tre variabili possibili";
+            break;
+
+        case (playerSelection === "sasso" && computerSelection === "forbici"):
+        case (playerSelection === "forbici" && computerSelection === "carta"):
+        case (playerSelection === "carta" && computerSelection === "sasso"):
+            return "Hai vinto!";
+            break;
+        
+        case (playerSelection === "sasso" && computerSelection === "carta"):
+        case (playerSelection === "carta" && computerSelection === "forbici"):
+        case (playerSelection === "forbici" && computerSelection === "sasso"):
+            return "Hai perso!";
+            break;
+        
+        default:
+            return "Pareggio!"
+    }
+}
+console.log(playRound())
 //funzione gioco
 //5 round e poi dichiara un vincitore
-
