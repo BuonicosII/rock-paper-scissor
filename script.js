42a93d21d85e2e30d9f730c8250118858ae74310
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-01-03 18:58:33 +0100

    Creata funzione random per sasso carta forbici

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //una funzione che restituisce carta sasso forbici per il computer
 
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    if (randomChoice === 1) {
+        return "Sasso"
+    } else if (randomChoice === 2) {
+        return "Carta"
+    } else {
+        return "Forbici"
+    }
+}
+
 //una funzione round che contiene
 //variabile sasso carta forbice da chiedere all'utente (prompt etc) -> case insensitive
 //variabile sasso carta forbice computer
@@ -7,6 +18,11 @@
 //condizioni di vittoria utente
 //condizioni di pareggio
 //tre messaggi
+
+function playRound() {
+    const playerSelection = prompt("Sasso, carta o forbici?", " ");
+    const computerSelection = getComputerChoice();
+}
 
 //funzione gioco
 //5 round e poi dichiara un vincitore
