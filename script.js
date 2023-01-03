303e7461279f2854aa35ec804369bbcdfe32eb10
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-01-03 18:27:32 +0100

    Scritto pseudocodice del gioco

---
+++ script.js
@@ -0,0 +1,13 @@
+//una funzione che restituisce carta sasso forbici per il computer
+
+//una funzione round che contiene
+//variabile sasso carta forbice da chiedere all'utente (prompt etc) -> case insensitive
+//variabile sasso carta forbice computer
+//condizioni di vittoria computer
+//condizioni di vittoria utente
+//condizioni di pareggio
+//tre messaggi
+
+//funzione gioco
+//5 round e poi dichiara un vincitore
+
